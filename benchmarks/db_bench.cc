1cf1c214bf404c45914e6d7f53591088d470a436
Baoquan <zhangbaoquan89@gmail.com>
2020-03-24 17:31:56 -0700

    Add stats in PMDB

--- benchmarks/db_bench.cc
+++ benchmarks/db_bench.cc
@@ -837,7 +837,7 @@ class Benchmark {
   void RangeRandom(ThreadState* thread) {
     ReadOptions options;
     int found = 0;
-    for (int i = 0; i < reads_/FLAGS_range_len; i++) {
+    for (int i = 0; i < reads_; i++) {
       Iterator* iter = db_->NewIterator(options);
       char key[100];
       const int k = thread->rand.Next() % FLAGS_num;
