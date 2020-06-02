46205afc19d626f91e3c624774415b65120d4587
Baoquan <zhangbaoquan89@gmail.com>
2020-06-01 19:26:04 -0700

    improve candidate list selection

--- db/btree_wrapper.h
+++ db/btree_wrapper.h
@@ -16,59 +16,65 @@
  * */
 namespace leveldb {
 
-  struct FileMetaData;
+#define NVM_WRITE_LATENCY_NS 500
+#define NVM_READ_LATENCY_NS 200
 
-  class btree_wrapper {
-  public:
-    btree_wrapper() : cur_key_("") {};
+struct FileMetaData;
+class Version;
 
-    /* Searching for a specific key
-     * Params:
-     *  key: key to search
-     *  sst_offset: A pair of sst id and block offset
-     * Return:
-     *  true if we find the key
-     * */
-    bool findKey(std::string key, std::pair<uint64_t, uint64_t>& sst_offset);
+class btree_wrapper {
+ public:
+  btree_wrapper()
+      : cur_key_(""),
+        nvm_read_latency_ns_(NVM_READ_LATENCY_NS),
+        nvm_write_latency_ns_(NVM_WRITE_LATENCY_NS){};
 
-    /* Inserting a key into the btree
-     * Params:
-     *  key : the key to insert
-     *  sst_id: the id of sst
-     *  block_offset : the offset of block containing the key
-     * */
-    void insertKey(std::string key, uint64_t sst_id, uint64_t block_offset,std::vector<FileMetaData*>** files_);
+  /* Searching for a specific key
+   * Params:
+   *  key: key to search
+   *  sst_offset: A pair of sst id and block offset
+   * Return:
+   *  true if we find the key
+   * */
+  bool findKey(std::string key, std::pair<uint64_t, uint64_t>& sst_offset);
 
-    void insertKeys(std::vector<std::string> keys,
-        std::vector<uint64_t> ssts,
-        std::vector<uint64_t> blocks,
-        std::vector<FileMetaData*>** files_);
+  /* Inserting a key into the btree
+   * Params:
+   *  key : the key to insert
+   *  sst_id: the id of sst
+   *  block_offset : the offset of block containing the key
+   * */
+  void insertKey(std::string key, uint64_t sst_id, uint64_t block_offset);
 
-    /* Return the current size of whole index
-     * */
-    uint64_t size() {
-      return global_tree.size();
-    };
+  void insertKeys(std::vector<std::string> keys, std::vector<uint64_t> ssts,
+                  std::vector<uint64_t> blocks);
 
-    std::string scanLeafnode(std::string cur_key, uint64_t num,
-                             std::vector<FileMetaData*>** files_);
+  /* Return the current size of whole index
+   * */
+  uint64_t size() { return global_tree.size(); };
 
-    uint64_t findSid(std::string key);
+  void erase(std::string key_to_delete) { global_tree.erase(key_to_delete); }
 
-    std::string getCurrentKey() {return cur_key_; }
-    void setCurrentKey(std::string key) { cur_key_ = key; }
+  std::string scanLeafnode(std::string cur_key, uint64_t num);
 
-    uint64_t get_mem_reads() { return mem_reads_; }
-    void reset_mem_reads() { mem_reads_ = 0;}
-    uint64_t get_mem_writes() { return mem_writes_;}
-    void reset_mem_writes() { mem_writes_ = 0; }
+  uint64_t findSid(std::string key);
 
-  private:
-    btree::btree_map<std::string, std::pair<uint64_t, uint64_t>> global_tree;
-    std::string cur_key_;
-    std::atomic<uint64_t> mem_reads_{0};
-    std::atomic<uint64_t> mem_writes_{0};
-  };
+  std::string getCurrentKey() { return cur_key_; }
+  void setCurrentKey(std::string key) { cur_key_ = key; }
+
+  uint64_t get_mem_reads() { return mem_reads_; }
+  void reset_mem_reads() { mem_reads_ = 0; }
+  uint64_t get_mem_writes() { return mem_writes_; }
+  void reset_mem_writes() { mem_writes_ = 0; }
+
+ private:
+  btree::btree_map<std::string, std::pair<uint64_t, uint64_t>> global_tree;
+  std::string cur_key_;
+  std::atomic<uint64_t> mem_reads_{0};
+  std::atomic<uint64_t> mem_writes_{0};
+  uint64_t nvm_write_latency_ns_;
+  uint64_t nvm_read_latency_ns_;
+};
 }
 
 #endif //LEVELDB_BTREE_WRAPPER_H
